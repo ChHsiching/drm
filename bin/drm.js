1747ad3f972dc46001a586aac20a8c51a39ad5f5
ChHsiching <hsichingchang@gmail.com>
2024-08-15 23:14:45 +0800

    Design of commands in bin file

--- bin/drm.js
+++ bin/drm.js
@@ -4,9 +4,14 @@
 import { Command } from 'commander';
 // chalk - Terminal string styling done right
 import chalk from 'chalk';
+// my custom commands
+import { useCommand } from '../src/commands/use.js';
+import { listCommand } from '../src/commands/list.js';
+
 
 // Basic CLI setup
 const program = new Command();
+
 program
   .version('1.0.0')
   .description('Docker Registry Manager (DRM)');
@@ -18,6 +23,18 @@ program
   .action(() => {
     console.log(chalk.green('DRM setup is working!'));
   });
+
+// Command to switch Docker mirror source
+program
+  .command('use <source-name>')
+  .description('Switch Docker mirror source')
+  .action(useCommand);
+
+// Command to list available Docker mirror source
+program
+  .command('list')
+  .description('List available Docker mirror sources')
+  .action(listCommand);
 
 // Parse command-line arguments
 program.parse(process.argv);
