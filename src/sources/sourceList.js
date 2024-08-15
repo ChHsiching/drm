5c4089d3e03a73d252a255a020eab20dcd6e8414
ChHsiching <hsichingchang@gmail.com>
2024-08-16 01:06:25 +0800

    Add docker source list

---
+++ src/sources/sourceList.js
@@ -0,0 +1,12 @@
+/**
+ * Predefined list of Docker mirror sources.
+ *
+ * @type {object}
+ */
+export const sources = {
+  'official': 'https://hub.docker.com',
+  'tencent': 'https://mirror.ccs.tencentyun.com',
+  'daocloud': 'https://www.daocloud.io/mirror',
+  'netease': 'https://hub-mirror.c.163.com',
+  'huawei': 'https://mirrors.huaweicloud.com'
+}
