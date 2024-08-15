7d1506aa31e1ad206510522bd63c7763eb669e6b
ChHsiching <hsichingchang@gmail.com>
2024-08-16 01:57:29 +0800

    Implement the validateSource()

---
+++ src/utils/validation.js
@@ -0,0 +1,18 @@
+import { axios } from "axios";
+
+/**
+ * Validates if the Docker mirror URL is reachable.
+ *
+ * @param {stinrg} mirrorUrl - The URL of the Docker mirror to validate.
+ * @returns {Promise<boolean>} - Returns true if the URL is reachable, otherwise false.
+ */
+export async function validateSource(mirrorUrl) {
+  try {
+    // Make a HEAD request to check if the URL is reachable
+    await axios.head(mirrorUrl);
+    return true;
+  } catch (error) {
+    // If there's an error (e.g., network issue, URL not reachable), return false
+    return false;
+  }
+}
