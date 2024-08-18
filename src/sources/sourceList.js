7e5b2c150eb0fe1b078af9d73795c5d7ca47a254
ChHsiching <hsichingchang@gmail.com>
2024-08-18 23:13:46 +0800

    Supplement the list of mirror sources

--- src/sources/sourceList.js
+++ src/sources/sourceList.js
@@ -12,9 +12,17 @@ export const sources = {
   'Netease': 'https://hub-mirror.c.163.com',
   'Tencent': 'https://mirror.ccs.tencentyun.com',
   'DockerProxy': 'https://dockerproxy.com',
+  'DockerPull': 'https://dockerpull.com',
   'Baidu': 'https://mirror.baidubce.com',
   'NJU': 'https://docker.nju.edu.cn',
   'SJTU': 'https://docker.mirrors.sjtug.sjtu.edu.cn',
   'ISCAS': 'https://mirror.iscas.ac.cn',
-  'Huawei': 'https://mirrors.huaweicloud.com'
+  'Huawei': 'https://mirrors.huaweicloud.com',
+  '1Panel': 'https://docker.1panel.live',
+  'Rat': 'https://hub.rat.dev',
+  'Dynadot': 'https://docker.registry.cyou',
+  'Alibaba': 'https://docker.rainbond.cc',
+  'Namecheap': 'https://dockerhub.icu',
+  'Zeruns': 'https://docker.awsl9527.cn',
+  'AtomHub': 'https://hub.atomgit.com'
 }
