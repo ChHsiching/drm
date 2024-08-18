6124b9f579ed4919ae926427dd2fbc0420ef37e0
ChHsiching <hsichingchang@gmail.com>
2024-08-18 18:26:38 +0800

    Add loading animation to the 'list' command

--- src/commands/list.js
+++ src/commands/list.js
@@ -5,7 +5,7 @@ import { sources } from "../sources/sourceList.js";
 /**
  * Checks if a source URL is available by making a simple HTTP reques url - The URL of the Docker mirror source.
  * @param {string} url - The URL of the Docker mirror source.
- * @returns {Promis<boolean>} - Resolves to true if the source is available, otherwise false.
+ * @returns {Promise<boolean>} - Resolves to true if the source is available, otherwise false.
  */
 async function isSourceAvailable(url) {
   try {
@@ -17,13 +17,47 @@ async function isSourceAvailable(url) {
 }
 
 /**
+ * Shows a spinning loader animation.
+ * @param {number} [interval=100] - Interval in milliseconds to update the spinner.
+ * @returns {function} - Function to clear the spinner animation.
+ */
+function showSpinner(interval = 100) {
+  const spinnerChars = ['|', '/', '-', '\\'];
+  let spinnerIndex = 0;
+
+  const spinnerInterval = setInterval(() => {
+    process.stdout.write(`\r${spinnerChars[spinnerIndex]} Checking...`);
+    spinnerIndex = (spinnerIndex + 1) % spinnerChars.length;
+  }, interval);
+
+  return () => {
+    clearInterval(spinnerInterval);
+    process.stdout.write('\r');
+  };
+}
+
+/**
+ * Clears the spinner animation.
+ * @param {number} spinnerInterval - The interval ID of the spinner animation.
+ */
+// function clearSpinner(spinnerInterval) {
+//   clearInterval(spinnerInterval);
+//   process.stdout.write('\rDone! \n');
+// }
+
+/**
  * Handler for the 'list' command.
  */
 export async function listCommand() {
   console.log('Available Docker mirror sources:');
 
   for (const [name, url] of Object.entries(sources)) {
+    const clearSpinner = showSpinner();
+    
     const available = await isSourceAvailable(url);
+
+    clearSpinner();
+    
     const status = available ? 'Available' : 'Unavailable';
     const color = available ? chalk.green : chalk.red;
     console.log(`- ${color(name)}: ${chalk.blue(url)} (${status})`);
