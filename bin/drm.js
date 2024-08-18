c245d10ce09ae11ae431ac54107e3763a65811ec
ChHsiching <hsichingchang@gmail.com>
2024-08-18 20:20:33 +0800

    Supplement the content of the drm command

--- bin/drm.js
+++ bin/drm.js
@@ -13,7 +13,7 @@ import { listCommand } from '../src/commands/list.js';
 const program = new Command();
 
 program
-  .version('1.0.0')
+  .version('1.0.0', '-v, --version')
   .description('Docker Registry Manager (DRM)');
 
 // Define a simple command
@@ -34,7 +34,21 @@ program
 program
   .command('list')
   .description('List available Docker mirror sources')
+  .alias('ls')
   .action(listCommand);
+
+// Display help information on unknown commands
+program
+  .on('command:*', (commands) => {
+    console.error(chalk.red(`Unknown command: ${commands[0]}`));
+    console.error('See --help for a list of available commands.');
+    program.help();
+  });
 
 // Parse command-line arguments
 program.parse(process.argv);
+
+// If no command was provided, display help information
+if (process.argv.length === 2) {
+  program.help();
+}
